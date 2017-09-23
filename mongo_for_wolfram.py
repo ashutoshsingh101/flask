fc90963c58f14c0c51e9d49ed125a048cf8df43f
ashutoshsingh101 <ashsingh6060@gmail.com>
2017-09-23 20:04:43 +0530

    msg

---
+++ mongo_for_wolfram.py
@@ -0,0 +1,27 @@
+import pymongo
+
+class mongo_db_database():
+	def __init__(self):
+		pass
+		
+	def connect_to_database(self,type_of_calculator,database_document):
+		uri = 'mongodb://ashutosh:nodedb123@ds139124.mlab.com:39124/health_personnel_google_places' 
+		client = pymongo.MongoClient(uri)
+		db = client.get_default_database()
+		if int(type_of_calculator) == 1:
+			db['weightLoss'].insert(database_document)
+		if int(type_of_calculator) == 2:
+			db['heartRisk'].insert(database_document)
+		if int(type_of_calculator) == 3:
+			db['physicalExercise'].insert(database_document)
+		if int(type_of_calculator) == 5:
+			db['bloodAlcoholContent'].insert(database_document)
+		client.close()
+		
+
+
+
+
+
+
+
